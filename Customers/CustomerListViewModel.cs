5cabed359b3fdcff97b198af527984248c468f78
Bartosz Migacz <4rchers@gmail.com>
2015-08-18 23:50:39 +0200

    viewlocator implemented

---
+++ Customers/CustomerListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+using MVVMHookupDemo.Services;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace MVVMHookupDemo.Customers
+{
+	public class CustomerListViewModel
+	{
+		private ICustomersRepository _repo = new CustomersRepository();
+		public CustomerListViewModel()
+		{
+			if(DesignerProperties.GetIsInDesignMode(
+				new System.Windows.DependencyObject()))
+				return;
+			Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result);
+			Debug.WriteLine(Customers.Count);
+		}
+		public ObservableCollection<Customer> Customers { get; set; }
+	}
+}
